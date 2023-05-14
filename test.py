dc123b753cf6f8033bd1323b09b3c364620a4e9e
mitanshudodia <80592354+mitanshudodia@users.noreply.github.com>
2023-05-14 13:29:37 +0530

    Update test.py for service endpoints

--- test.py
+++ test.py
@@ -1,22 +1,22 @@
 import requests
 import base64
 
-API_URL = 'http://0.0.0.0:8000/zero'
-headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/json"}
+#API_URL = 'http://0.0.0.0:8000/zero'
+#this is the endpoints of text generatino server hosted as as a service on TrueFoundry
+API_URL = 'https://fastapi-intern-mitanshu-8000.demo1.truefoundry.com/zero'
 
-
-def query(data):
+def query():
     
-    #input for zero shot classification
-    input_data = {'inputs': 'Hi, I recently bought a device from your company but it is not working as advertised and I would like to get reimbursed!',
-                  'candidate_labels': ["refund", "legal", "faq"]}
+ #input for zero shot classification
+#     input_data = {'
+# 	inputs': 'Hi, I recently bought a device from your company but it is not working as advertised and I would like to get reimbursed!',
+#       'candidate_labels': ["refund", "legal", "faq"]
+# 	}
 
     #input for token classification and text genetation
-    # input_data = {
-	# "inputs": "My name is Sarah Jessica Parker but you can call me Jessica and I am studying computer science engineering in Mumbai",
-    # }
+    input_data = {
+	"inputs": "My name is Sarah Jessica Parker but you can call me Jessica and I am studying computer science engineering in Mumbai",
+    }
 
     #input for object detection
     # input_data = {'inputs': data}
